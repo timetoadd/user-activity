456623992a62e42454d2e66174a23bcef57e7b63
timetoadd <7689172+timetoadd@users.noreply.github.com>
2023-04-29 08:06:58 -0400

    3 files modified

--- src/DeveloperProfile.js
+++ src/DeveloperProfile.js
@@ -10,7 +10,20 @@ const DeveloperProfile = ({ id, developers }) => {
       <div className="developer-profile-container">
         <h2 className="developer-name">{developers[id - 1].name}'s Profile</h2>
         <p className="developer-skills">{developers[id - 1].bio}</p>
+        <p className="developer-profile">
+          Profile:{' '}
+          <a href={developers[id - 1].profile} target="_blank">
+            {developers[id - 1].profile}
+          </a>
+        </p>
+        <p className="developer-ads">ADS: {developers[id - 1].id}</p>
         <p className="developer-email">Email: {developers[id - 1].email}</p>
+        <p className="developer-github-profile">
+          Github:{' '}
+          <a href={developers[id - 1].githubProfile} target="_blank">
+            {developers[id - 1].githubProfile}
+          </a>
+        </p>
       </div>
       <br />
       <div className="developer-profile-container">
